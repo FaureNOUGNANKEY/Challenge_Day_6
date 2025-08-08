a43ee6b85d21e3f00ccb0b4312176d00bc17c11e
Faure NOUGNANKEY <na5yfaure@gmail.com>
2025-08-08 22:22:10 +0000

    jour_17

---
+++ jour_17.py
@@ -0,0 +1,8 @@
+# Exercises: Day 17
+# 1. 
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries , es , ru = names
+
+print ("nordic_countries:",nordic_countries )
+print ("es:" , es)
+print ("ru:", ru)
